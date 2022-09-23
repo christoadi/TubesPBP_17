5fd1af7ff767a741499200343e0e5a7170e9d0ff
christoadi <chrizto.adi456@gmail.com>
2022-09-23 23:47:40 +0700

    UGD1

--- app/src/main/java/com/example/ugd1/room/UserDao.kt
+++ app/src/main/java/com/example/ugd1/room/UserDao.kt
@@ -12,9 +12,6 @@ interface UserDao {
     suspend fun deleteUser(note: User)
     @Query("SELECT * FROM User")
     suspend fun getUser() : List<User>
-    @Query("SELECT * FROM User WHERE name =:user_name")
+    @Query("SELECT * FROM User WHERE username =:user_name")
     suspend fun getUser(user_name: String) : List<User>
-    @Query("SELECT * FROM User WHERE number =:user_number")
-    suspend fun getUser(user_number: Int) : List<User>
-
 }
