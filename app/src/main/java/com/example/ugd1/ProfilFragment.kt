c1dab04cd624976fc5b93967b964e8164acb0515
christoadi <chrizto.adi456@gmail.com>
2022-09-25 00:22:14 +0700

    UGD1

--- app/src/main/java/com/example/ugd1/ProfilFragment.kt
+++ app/src/main/java/com/example/ugd1/ProfilFragment.kt
@@ -29,7 +29,7 @@ class ProfilFragment : Fragment() {
         setProfil()
 
         binding.btnUpdate.setOnClickListener {
-
+            transitionFragment(EditProfileFragment())
         }
     }
 
