19501ad9d80715e440fbae3fa895cfaffc8d4ef4
christoadi <chrizto.adi456@gmail.com>
2022-10-01 18:10:24 +0700

    UGD1

--- app/src/main/java/com/example/ugd1/EditMemberGym.kt
+++ app/src/main/java/com/example/ugd1/EditMemberGym.kt
@@ -1,8 +1,20 @@
 package com.example.ugd1
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.example.ugd1.databinding.ActivityEditMemberGymBinding
 import com.example.ugd1.room.Constant
 import com.example.ugd1.room.MemberGym
 import com.example.ugd1.room.MemberGymDB
@@ -14,6 +26,9 @@ import kotlinx.coroutines.launch
 class EditMemberGym : AppCompatActivity() {
     val db by lazy { MemberGymDB(this) }
     private var memberGymId : Int = 0
+    private var binding: ActivityEditMemberGymBinding? = null
+    private val MEMBER_GYM_1 = "notif_member_gym_1"
+    private val notificationId = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +56,56 @@ class EditMemberGym : AppCompatActivity() {
         }
     }
 
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+
+            val channel1 = NotificationChannel(MEMBER_GYM_1, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+            }
+
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel1)
+        }
+    }
+
+    private fun sendNotification() {
+//        val intent : Intent = Intent(this, MainActivity::class.java).apply {
+//            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//        }
+//
+//        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+//        val broadcastIntent : Intent = Intent(this, NotificationReceiver::class.java)
+//        broadcastIntent.putExtra("toastMessage", "Register Succesful, Happy Gym "+binding?.inputUsername?.text.toString())
+//        val actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val logo = BitmapFactory.decodeResource(resources, R.drawable.gym)
+        val builder = NotificationCompat.Builder(this, MEMBER_GYM_1)
+            .setSmallIcon(R.drawable.gym)
+            .setContentTitle("Edit Member Gym")
+            .setContentText("Success Edit as "+ binding?.etMembership?.text.toString())
+//            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setColor(Color.BLUE)
+            .setLargeIcon(logo)
+            .setStyle(
+                NotificationCompat.BigPictureStyle()
+                    .bigLargeIcon(null)
+                    .bigPicture(logo)
+            )
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+//            .setContentIntent(pendingIntent)
+//            .addAction(R.mipmap.ic_launcher, "Welcome", actionIntent)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build())
+        }
+    }
+
     private fun setupListener() {
         btnSave.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
@@ -52,6 +117,8 @@ class EditMemberGym : AppCompatActivity() {
                         etDurasi.text.toString()
                     )
                 )
+                createNotificationChannel()
+                sendNotification()
                 finish()
             }
         }
@@ -64,6 +131,8 @@ class EditMemberGym : AppCompatActivity() {
                         etTanggal.text.toString(),
                         etDurasi.text.toString())
                 )
+                createNotificationChannel()
+                sendNotification()
                 finish()
             }
         }
--- app/src/main/java/com/example/ugd1/MemberGymFragment.kt
+++ app/src/main/java/com/example/ugd1/MemberGymFragment.kt
@@ -1,11 +1,16 @@
 package com.example.ugd1
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 
 class MemberGymFragment : Fragment() {
@@ -16,6 +21,7 @@ class MemberGymFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_member_gym, container, false)
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
