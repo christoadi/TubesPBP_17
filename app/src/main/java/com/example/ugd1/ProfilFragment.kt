8232a30e3adca44044bed22b7dbb2758181f34e8
christoadi <chrizto.adi456@gmail.com>
2022-11-21 21:21:07 +0700

    Nomor 3 UGD11

--- app/src/main/java/com/example/ugd1/ProfilFragment.kt
+++ app/src/main/java/com/example/ugd1/ProfilFragment.kt
@@ -8,14 +8,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.ugd1.api.UserApi
 import com.example.ugd1.databinding.FragmentProfilBinding
 import com.example.ugd1.room.UserDB
+import org.json.JSONObject
+import java.nio.charset.StandardCharsets
 
 
 class ProfilFragment : Fragment() {
 
     private var _binding  : FragmentProfilBinding?= null
     private val binding get() = _binding!!
+    private var queue: RequestQueue? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,7 +39,11 @@ class ProfilFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setProfil()
+
+        queue = Volley.newRequestQueue(activity)
+        val sharedPreferences = (activity as HomeActivity).getSharedPreferences()
+        var id = sharedPreferences.getInt("id", 0)
+        showUser(id)
 
         binding.btnUpdate.setOnClickListener {
             transitionFragment(EditProfileFragment())
@@ -71,6 +85,44 @@ class ProfilFragment : Fragment() {
         transition.hide(ProfilFragment())
     }
 
+    private fun showUser(id: Int) {
+//        setLoading(true)
+        val stringRequest: StringRequest = object :
+            StringRequest(Method.GET, UserApi.GET_BY_ID_URL + id, Response.Listener { response ->
+                // val gson = Gson()
+                // val mahasiswa = gson.fromJson(response, Mahasiswa::class.java)
 
+                var userObject = JSONObject(response.toString())
+                val userData = userObject.getJSONObject("data")
+
+                binding.viewUsername.setText(userData.getString("username"))
+                binding.viewEmail.setText(userData.getString("email"))
+                binding.viewNomorTelepon.setText(userData.getString("nomorTelepon"))
+
+                Toast.makeText(activity, "Data User berhasil diambil!", Toast.LENGTH_SHORT).show()
+//                setLoading(false)
+            }, Response.ErrorListener { error ->
+//                setLoading(false)
+                try {
+                    val responseBody = String(error.networkResponse.data, StandardCharsets.UTF_8)
+                    val errors = JSONObject(responseBody)
+                    Toast.makeText(
+                        activity,
+                        errors.getString("message"),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }catch (e: Exception) {
+                    Toast.makeText(activity, e.message, Toast.LENGTH_SHORT).show()
+                }
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Accept"] = "application/json"
+                return headers
+            }
+        }
+        queue!!.add(stringRequest)
+    }
 
 }
