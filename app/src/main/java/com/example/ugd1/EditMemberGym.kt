d2a5e773b046432868d0d02a77ddb7b1e2a1164b
wisnawa <putuwisnawaandrian@yahoo.co.id>
2022-11-22 21:26:00 +0700

    Done

--- app/src/main/java/com/example/ugd1/EditMemberGym.kt
+++ app/src/main/java/com/example/ugd1/EditMemberGym.kt
@@ -11,178 +11,254 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
+import android.widget.*
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
-import com.example.ugd1.databinding.ActivityEditMemberGymBinding
+import com.android.volley.AuthFailureError
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.ugd1.api.MemberGymApi
+import com.example.ugd1.model.MemberGymModel
 import com.example.ugd1.room.*
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_edit_member_gym.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.nio.charset.StandardCharsets
 
 class EditMemberGym : AppCompatActivity() {
-    val db by lazy { MemberGymDB(this) }
-    val MemberGymDao = db.MemberGymDao()
 
-    private var memberGymId : Int = 0
-    private var binding: ActivityEditMemberGymBinding? = null
-    private val MEMBER_GYM_1 = "notif_member_gym_1"
-    private val notificationId = 1
-
+    private var etId: EditText? = null
+    private var etPersonalTrainer: EditText? = null
+    private var etMembership: EditText? = null
+    private var edTanggal: EditText? = null
+    private var edDurasi: EditText? = null
+    private var layoutLoading: LinearLayout? = null
+    private var queue: RequestQueue? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_member_gym)
-        supportActionBar?.hide()
-        setupView()
-        setupListener()
-    }
 
-    fun setupView() {
-        val intentType = intent.getIntExtra("intent_type", 0)
-        when (intentType){
-            Constant.TYPE_CREATE-> {
-                btnUpdate.visibility = View.GONE
-            }
-            Constant.TYPE_READ-> {
-                btnSave.visibility = View.GONE
-                btnUpdate.visibility = View.GONE
-                getMemberGym()
-            }
-            Constant.TYPE_UPDATE-> {
-                btnSave.visibility = View.GONE
-                getMemberGym()
-            }
-        }
-    }
-
-
-    private fun createNotificationChannel() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Notification Title"
-            val descriptionText = "Notification Description"
-
-            val channel1 = NotificationChannel(MEMBER_GYM_1, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
-                description = descriptionText
-            }
-
-
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel1)
-        }
-    }
-
-    private fun sendNotification2() {
-//        val intent : Intent = Intent(this, MainActivity::class.java).apply {
-//            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//        }
+        //Pendeklarasian request queue
+        queue = Volley.newRequestQueue(this)
+        etId = findViewById(R.id.et_id)
+        etPersonalTrainer = findViewById(R.id.et_personalTrainer)
+        etMembership = findViewById(R.id.et_membership)
+        edTanggal = findViewById(R.id.et_tanggal)
+        edDurasi = findViewById(R.id.et_durasi)
+        layoutLoading = findViewById(R.id.layout_loading)
 //
-//        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-//        val broadcastIntent : Intent = Intent(this, NotificationReceiver::class.java)
-//        broadcastIntent.putExtra("toastMessage", "Register Succesful, Happy Gym "+binding?.inputUsername?.text.toString())
-//        val actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-        val logo = BitmapFactory.decodeResource(resources, R.drawable.gym)
-        val builder = NotificationCompat.Builder(this, MEMBER_GYM_1)
-            .setSmallIcon(R.drawable.gym)
-            .setContentTitle("Edit Member Gym")
-            .setContentText("Success Edit as "+ binding?.etMembership?.text.toString())
-//            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-            .setColor(Color.BLUE)
-            .setLargeIcon(logo)
-            .setStyle(
-                NotificationCompat.BigPictureStyle()
-                    .bigLargeIcon(null)
-                    .bigPicture(logo)
-            )
-            .setAutoCancel(true)
-            .setOnlyAlertOnce(true)
-//            .setContentIntent(pendingIntent)
-//            .addAction(R.mipmap.ic_launcher, "Welcome", actionIntent)
-            .setPriority(NotificationCompat.PRIORITY_LOW)
+//        setExposedDropDownMenu()
 
-        with(NotificationManagerCompat.from(this)) {
-            notify(notificationId, builder.build())
+        val btnCancel = findViewById<Button>(R.id.btn_cancel)
+        btnCancel.setOnClickListener{finish() }
+        val btnSave = findViewById<Button>(R.id.btn_save)
+        val tvTitle =findViewById<TextView>(R.id.tv_title)
+        val id = intent.getIntExtra("id", -1)
+        if(id== -1){
+            tvTitle.setText("Tambah Member Gym")
+            btnSave.setOnClickListener { createMahasiswa() }
+        }else{
+            tvTitle.setText("Edit Member Gym")
+            getMahasiswaById(id)
+
+            btnSave.setOnClickListener { updateMahasiswa(id) }
         }
+
     }
 
-    private fun sendNotification1() {
-//        val intent : Intent = Intent(this, MainActivity::class.java).apply {
-//            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//        }
+//    fun setExposedDropDownMenu(){
+//        val adapterFakultas: ArrayAdapter<String> = ArrayAdapter<String>(this,
+//            R.layout.item_list, FAKULTAS_LIST)
+//        edFakultas!!.setAdapter(adapterFakultas)
 //
-//        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-//        val broadcastIntent : Intent = Intent(this, NotificationReceiver::class.java)
-//        broadcastIntent.putExtra("toastMessage", "Register Succesful, Happy Gym "+binding?.inputUsername?.text.toString())
-//        val actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-        val logo = BitmapFactory.decodeResource(resources, R.drawable.gym)
-        val builder = NotificationCompat.Builder(this, MEMBER_GYM_1)
-            .setSmallIcon(R.drawable.gym)
-            .setContentTitle("Add Member Gym")
-            .setContentText("Success Add as "+ binding?.etMembership?.text.toString())
-            .setColor(Color.BLUE)
-            .setLargeIcon(logo)
-            .setStyle(
-                NotificationCompat.BigPictureStyle()
-                    .bigLargeIcon(null)
-                    .bigPicture(logo)
+//        val adapterProdi: ArrayAdapter<String> = ArrayAdapter<String>(this,
+//            R.layout.item_list, PRODI_LIST)
+//        edProdi!!.setAdapter(adapterProdi)
+//    }
+
+    private fun getMahasiswaById(id: Int){
+        // Fungsi untuk menampilkan data mahasiswa berdasarkan id
+        setLoading(true)
+        val stringRequest: StringRequest =
+            object : StringRequest(Method.GET, MemberGymApi.GET_BY_ID_URL + id, Response.Listener { response ->
+//                val gson = Gson()
+//                val member = gson.fromJson(response, MemberGym::class.java)
+
+                var memberJo = JSONObject(response.toString())
+                val member = memberJo.getJSONObject("data")
+
+                etPersonalTrainer!!.setText(member.getString("personalTrainer"))
+                etMembership!!.setText(member.getString("membership"))
+                edTanggal!!.setText(member.getString("tanggal"))
+                edDurasi!!.setText(member.getString("durasi"))
+
+                Toast.makeText(this@EditMemberGym, "Data berhasil diambil", Toast.LENGTH_SHORT).show()
+                setLoading(false)
+            },  Response.ErrorListener { error ->
+                setLoading(false)
+                try{
+                    val responseBody = String(error.networkResponse.data, StandardCharsets.UTF_8)
+                    val errors = JSONObject(responseBody)
+                    Toast.makeText(
+                        this@EditMemberGym,
+                        errors.getString("message"),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }catch (e: Exception){
+                    Toast.makeText(this@EditMemberGym, e.message, Toast.LENGTH_SHORT).show()
+                }
+            }){
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["Accept"] = "application/json"
+                    return headers
+                }
+            }
+        queue!!.add(stringRequest)
+    }
+
+    private fun createMahasiswa(){
+        setLoading(true)
+
+        val member = MemberGymModel(
+            0,
+            etPersonalTrainer!!.text.toString(),
+            etMembership!!.text.toString(),
+            edTanggal!!.text.toString(),
+            edDurasi!!.text.toString()
+        )
+        val stringRequest: StringRequest =
+            object: StringRequest(Method.POST, MemberGymApi.ADD_URL, Response.Listener { response->
+                val gson = Gson()
+                val mahasiswa = gson.fromJson(response, MemberGym::class.java)
+
+                if(mahasiswa!=null)
+                    Toast.makeText(this@EditMemberGym, "Data Berhasil Ditambahkan", Toast.LENGTH_SHORT).show()
+
+                val returnIntent = Intent()
+                setResult(RESULT_OK, returnIntent)
+                finish()
+
+                setLoading(false)
+            }, Response.ErrorListener { error->
+                setLoading(false)
+                try{
+                    val responseBody = String(error.networkResponse.data, StandardCharsets.UTF_8)
+                    val errors = JSONObject(responseBody)
+                    Toast.makeText(
+                        this@EditMemberGym,
+                        errors.getString("message"),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }catch (e:Exception){
+                    Toast.makeText(this@EditMemberGym, e.message, Toast.LENGTH_SHORT).show()
+                }
+            }) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["Accept"] = "application/json"
+                    return headers
+
+                }
+
+                @Throws(AuthFailureError::class)
+                override fun getBody(): ByteArray {
+                    val gson = Gson()
+                    val requestBody = gson.toJson(member)
+                    return requestBody.toByteArray(StandardCharsets.UTF_8)
+                }
+
+                override fun getBodyContentType(): String {
+                    return "application/json"
+                }
+            }
+        // Menambahkan request ke request queue
+        queue!!.add(stringRequest)
+    }
+
+    private fun updateMahasiswa(id: Int){
+        setLoading(true)
+
+        val member = MemberGymModel(
+            id,
+            etPersonalTrainer!!.text.toString(),
+            etMembership!!.text.toString(),
+            edTanggal!!.text.toString(),
+            edDurasi!!.text.toString()
+        )
+
+        val stringRequest: StringRequest = object :
+            StringRequest(Method.PUT, MemberGymApi.UPDATE_URL + id, Response.Listener{ response ->
+                val gson = Gson()
+
+                val mahasiswa = gson.fromJson(response, MemberGymModel::class.java)
+
+                if(mahasiswa != null)
+                    Toast.makeText(this@EditMemberGym, "Data Berhasil Diupdate", Toast.LENGTH_SHORT).show()
+                val returnIntent = Intent()
+                setResult(RESULT_OK, returnIntent)
+                finish()
+
+                setLoading(false)
+            }, Response.ErrorListener{ error->
+                setLoading(false)
+                try{
+                    val responseBody = String(error.networkResponse.data, StandardCharsets.UTF_8)
+                    val errors = JSONObject(responseBody)
+                    Toast.makeText(
+                        this@EditMemberGym,
+                        errors.getString("message"),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }catch (e:Exception){
+                    Toast.makeText(this@EditMemberGym, e.message, Toast.LENGTH_SHORT).show()
+                }
+            }){
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Accept"] = "application/json"
+                return headers
+
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getBody(): ByteArray {
+                val gson = Gson()
+                val requestBody = gson.toJson(member)
+                return requestBody.toByteArray(StandardCharsets.UTF_8)
+            }
+
+            override fun getBodyContentType(): String {
+                return "application/json"
+            }
+        }
+        queue!!.add(stringRequest)
+
+    }
+
+    //Fungsi ini digunakan untuk menampilkan layout Loading
+
+    private fun setLoading(isLoading: Boolean){
+        if(isLoading){
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
             )
-            .setAutoCancel(true)
-            .setOnlyAlertOnce(true)
-//            .setContentIntent(pendingIntent)
-//            .addAction(R.mipmap.ic_launcher, "Welcome", actionIntent)
-            .setPriority(NotificationCompat.PRIORITY_LOW)
-
-        with(NotificationManagerCompat.from(this)) {
-            notify(notificationId, builder.build())
+            layoutLoading!!.visibility = View.VISIBLE
+        }else{
+            window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+            layoutLoading!!.visibility = View.INVISIBLE
         }
-    }
-
-    private fun setupListener() {
-        btnSave.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                db.MemberGymDao().addMemberGym(
-                    MemberGym(
-                        0, etPersonalTrainer.text.toString(),
-                        etMembership.text.toString(),
-                        etTanggal.text.toString(),
-                        etDurasi.text.toString()
-                    )
-                )
-                createNotificationChannel()
-                sendNotification1()
-                finish()
-            }
-        }
-        btnUpdate.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                db.MemberGymDao().updateMemberGym(
-                    MemberGym(memberGymId,
-                        etPersonalTrainer.text.toString(),
-                        etMembership.text.toString(),
-                        etTanggal.text.toString(),
-                        etDurasi.text.toString())
-                )
-                createNotificationChannel()
-                sendNotification2()
-                finish()
-            }
-        }
-    }
-    fun getMemberGym(){
-        memberGymId = intent.getIntExtra("intent_id", 0)
-        CoroutineScope(Dispatchers.IO).launch {
-            val memberGym = db.MemberGymDao().getMemberGym(memberGymId)[0]
-            etPersonalTrainer.setText(memberGym.personalTrainer)
-            etMembership.setText(memberGym.membership)
-            etTanggal.setText(memberGym.tanggal)
-            etDurasi.setText(memberGym.durasi)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/ugd1/MemberGymAdapter.kt
+++ app/src/main/java/com/example/ugd1/MemberGymAdapter.kt
@@ -1,59 +1,127 @@
 package com.example.ugd1
 
-import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ugd1.model.MemberGymModel
 import com.example.ugd1.room.MemberGym
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_member_gym_adapter.view.*
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MemberGymAdapter (private val memberGyms : ArrayList<MemberGym>, private val
-listener: OnAdapterListener
-) :
-    RecyclerView.Adapter<MemberGymAdapter.MemberGymViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-            MemberGymViewHolder {
-        return MemberGymViewHolder(
+class MemberGymAdapter(private var memberGymList: List<MemberGymModel>, context: Context) :
+    RecyclerView.Adapter<MemberGymAdapter.ViewHolder>(), Filterable {
 
-            LayoutInflater.from(parent.context).inflate(R.layout.activity_member_gym_adapter, parent, false)
-        )
+    private var filteredMemberList: MutableList<MemberGymModel>
+    private val context: Context
+
+    init{
+        filteredMemberList = ArrayList(memberGymList)
+        this.context=context
     }
 
-    override fun onBindViewHolder(
-        holder: MemberGymViewHolder, position:
-        Int ) {
-        val memberGym = memberGyms[position]
-        holder.view.tvPersonalTrainer.text = memberGym.personalTrainer
-        holder.view.tvMembership.text = memberGym.membership
-        holder.view.tvTanggal.text = memberGym.tanggal
-        holder.view.tvDurasi.text = memberGym.durasi
-        holder.view.tvPersonalTrainer.setOnClickListener {
-            listener.onClick(memberGym)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_member, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return filteredMemberList.size
+    }
+
+    fun setMemberGymList(memberGymList: Array<MemberGymModel>){
+        this.memberGymList = memberGymList.toList()
+        filteredMemberList = memberGymList.toMutableList()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int){
+        val member = filteredMemberList[position]
+//        holder.tvId.text = member.id.toString()
+        holder.tvPersonalTrainer.text = member.personalTrainer
+        holder.tvMembership.text = member.membership
+        holder.tvTanggal.text = member.tanggal
+        holder.tvDurasi.text = member.durasi
+
+        holder.btnDelete.setOnClickListener{
+            val materialAlertDialogBuilder = MaterialAlertDialogBuilder(context)
+            materialAlertDialogBuilder.setTitle("Konfirmasi")
+                .setMessage("Apakah anda yakin ingin menghapus data mahasiswa ini?")
+                .setNegativeButton("Batal", null)
+                .setPositiveButton("Hapus"){_, _ ->
+                    if(context is MemberGymActivity){
+                        context.deleteMemberGym(
+                            member.id
+                        )
+                    }
+                }
+                .show()
         }
-        holder.view.icon_edit.setOnClickListener {
-            listener.onUpdate(memberGym)
+        holder.cvMahasiswa.setOnClickListener{
+            val i = Intent(context, EditMemberGym::class.java)
+            i.putExtra("id",member.id)
+            if(context is MemberGymActivity)
+                context.startActivityForResult(i, MemberGymActivity.LAUNCH_ADD_ACTIVITY)
+
         }
-        holder.view.icon_delete.setOnClickListener {
-            listener.onDelete(memberGym)
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter(){
+            override fun performFiltering(charSequence: CharSequence): FilterResults {
+                val charSequenceString = charSequence.toString()
+                val filtered : MutableList<MemberGymModel> = java.util.ArrayList()
+                if(charSequenceString.isEmpty()){
+                    filtered.addAll(memberGymList)
+                }else{
+                    for(member in memberGymList){
+                        if(member.personalTrainer.lowercase(Locale.getDefault())
+                                .contains(charSequenceString.lowercase(Locale.getDefault()))
+                        ) filtered.add(member)
+
+                    }
+                }
+                val filterResults = FilterResults()
+                filterResults.values = filtered
+                return filterResults
+            }
+
+            override fun publishResults(charSequence: CharSequence, filterResults: FilterResults) {
+                filteredMemberList.clear()
+                filteredMemberList.addAll((filterResults.values as List<MemberGymModel>))
+                notifyDataSetChanged()
+            }
         }
     }
 
-    override fun getItemCount() = memberGyms.size
-    inner class MemberGymViewHolder(val view: View) :
-        RecyclerView.ViewHolder(view)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+//        var tvId: TextView
+        var tvPersonalTrainer: TextView
+        var tvMembership: TextView
+        var tvTanggal: TextView
+        var tvDurasi: TextView
+        var btnDelete: ImageButton
+        var cvMahasiswa: CardView
 
-    @SuppressLint("NotifyDataSetChanged")
-    fun setData(list: List<MemberGym>) {
-        memberGyms.clear()
-        memberGyms.addAll(list)
-        notifyDataSetChanged()
+        init{
+//            tvId = itemView.findViewById(R.id.tv_id)
+            tvPersonalTrainer = itemView.findViewById(R.id.tv_personalTrainer)
+            tvMembership = itemView.findViewById(R.id.tv_membership)
+            tvTanggal = itemView.findViewById(R.id.tv_tanggal)
+            tvDurasi = itemView.findViewById(R.id.tv_durasi)
+            btnDelete = itemView.findViewById(R.id.btn_delete)
+            cvMahasiswa = itemView.findViewById(R.id.cv_mahasiswa)
+        }
     }
 
-    interface OnAdapterListener {
-        fun onClick(memberGym: MemberGym)
-        fun onUpdate(memberGym: MemberGym)
-        fun onDelete(memberGym: MemberGym)
-
-    }
 }
