ac1bee59b31033e7cdbbfa40006262b5c2d7b77d
christoadi <chrizto.adi456@gmail.com>
2022-11-27 16:27:30 +0700

    UGD12 UI (1)

--- app/src/main/java/com/example/ugd1/HomeActivity.kt
+++ app/src/main/java/com/example/ugd1/HomeActivity.kt
@@ -4,9 +4,14 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_home.*
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
@@ -19,38 +24,70 @@ class HomeActivity : AppCompatActivity() {
         setTitle("User Menu")
 
         getSupportActionBar()?.hide()
-        var bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
+//        var bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
         val home = HomeFragment()
         val loc = LocationFragment()
         val memberGym = MemberGymFragment()
         val profile = ProfilFragment()
 
         setThatFragments(home)
+//
+//        bottomNavigationView.setOnItemSelectedListener{
+//            when(it.itemId){
+//                R.id.navigation_home -> {
+//                    setThatFragments(home)
+//                }
+//                R.id.navigation_location -> {
+//                    setThatFragments(loc)
+//                }
+//                R.id.navigation_memberGym -> {
+//                    setThatFragments(memberGym)
+//                }
+//                R.id.navigation_profile -> {
+//                    setThatFragments(profile)
+//                }
+//            }
+//            true
+//        }
 
-        bottomNavigationView.setOnItemSelectedListener{
-            when(it.itemId){
-                R.id.navigation_home -> {
-                    setThatFragments(home)
+        nav_view.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
+            override fun onTabSelected(
+                lastIndex: Int,
+                lastTab: AnimatedBottomBar.Tab?,
+                newIndex: Int,
+                newTab: AnimatedBottomBar.Tab
+            ) {
+
+                //redirecting fragments
+                when(newIndex){
+                    0 -> setThatFragments(home);
+                    1 -> setThatFragments(loc);
+                    2 -> setThatFragments(memberGym);
+                    3 -> setThatFragments(profile);
                 }
-                R.id.navigation_location -> {
-                    setThatFragments(loc)
-                }
-                R.id.navigation_memberGym -> {
-                    setThatFragments(memberGym)
-                }
-                R.id.navigation_profile -> {
-                    setThatFragments(profile)
-                }
+                Log.d("bottom_bar", "Selected index: $newIndex, title: ${newTab.title}")
             }
-            true
-        }
+
+            override fun onTabReselected(index: Int, tab: AnimatedBottomBar.Tab) {
+                Log.d("bottom_bar", "Reselected index: $index, title: ${tab.title}")
+            }
+        });
     }
+
+
+
 
     private fun setThatFragments(fragment : Fragment){
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.layoutFragment,fragment)
             commit()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuInflater = MenuInflater(this)
+        menuInflater.inflate(R.menu.nav_home,menu)
+        return true
     }
 
     fun getSharedPreferences(): SharedPreferences{
